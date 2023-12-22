59db2a19cdf5411c1c5a3a42469bc96ed97fd503
Donna100 <donia.laajili@supinfo.com>
2023-12-22 16:20:49 +0100

    update arborescence

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',  // Fichier d'entrée de votre application
+  entry: './app/vu.js',  // Fichier d'entrée de votre application
   output: {
     filename: 'bundle.js',  // Nom du fichier de sortie
     path: path.resolve(__dirname, 'dist'),  // Dossier de sortie
