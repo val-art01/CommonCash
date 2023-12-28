e19c1ee3bcdaf286b3094a9636c9e4149c86a053
Donna100 <donia.laajili@supinfo.com>
2023-12-28 16:02:41 +0100

    update home

--- Frontend/web/src/views/Home.vue
+++ Frontend/web/src/views/Home.vue
@@ -1,19 +1,50 @@
 <!-- src/views/Home.vue -->
+<!-- Home.vue -->
+
 <template>
-    <div>
-      <navbar></navbar>
-      <div>
-        <h1>Bienvenue sur l'application de gestion de dépenses</h1>
+  <div>
+    <navbar></navbar>
+    <div class="welcome-section">
+      <h1>CommonCash</h1>
+      <h2>Bienvenue sur l'application de gestion de dépenses</h2>
+      <p>Connectez-vous pour commencer à utiliser l'application.</p>
+
+      <div class="buttons">
+        <router-link to="/login">
+          <button class="login-button">Se connecter</button>
+        </router-link>
+        <router-link to="/register">
+          <button class="register-button">S'inscrire</button>
+        </router-link>
       </div>
     </div>
+  </div>
 </template>
-  
+
 <script>
-  export default {
-    name: 'Home',
-  };
+export default {
+  name: 'Home',
+};
 </script>
-  
+
 <style scoped>
-  /* Styles spécifiques à la page d'accueil */
+/* Styles spécifiques à la page d'accueil */
+
+.welcome-section {
+  text-align: center;
+  margin: 50px;
+}
+
+.buttons {
+  margin-top: 20px;
+}
+
+.login-button,
+.register-button {
+  padding: 10px 20px;
+  font-size: 16px;
+  margin: 0 10px;
+  cursor: pointer;
+}
+
 </style>
