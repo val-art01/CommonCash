728b682358e5ee64a3389df1ad30696644a8249d
Donna100 <donia.laajili@supinfo.com>
2024-01-04 03:43:59 +0100

    update dashboard

--- Frontend/web/src/routes/index.js
+++ Frontend/web/src/routes/index.js
@@ -4,12 +4,14 @@ import Home from '@/views/Home.vue';
 import Login from '@/views/Login.vue';
 import Register from '@/views/Register.vue';
 import MobileExplanation from '@/views/application.vue';
+import Dashboard from '@/views/dashboard.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/login', name: 'Login', component: Login },
   { path: '/register', name: 'Register', component: Register },
   { path: '/application', name: 'MobileExplanation', component: MobileExplanation },
+  { path: '/dashboard', name: 'Dashboard',  component: Dashboard },
   // Ajoutez d'autres routes au besoin
 ];
 
---
+++ Frontend/web/src/views/dashboard.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+      <!-- En-tête du tableau de bord -->
+      <div class="dashboard-header">
+        <h1>Bienvenue dans votre Tableau de Bord</h1>
+        <button @click="logout">Déconnexion</button>
+      </div>
+  
+      <!-- Widgets du tableau de bord -->
+      <div class="dashboard-widgets">
+        <!-- Widget 1 : Liste des dépenses récentes -->
+        <div class="dashboard-widget">
+          <h2>Dépenses Récentes</h2>
+          <!-- Contenu du widget (liste des dépenses récentes) -->
+        </div>
+  
+        <!-- Widget 2 : Graphique des revenus et dépenses -->
+        <div class="dashboard-widget">
+          <h2>Graphique des Revenus et Dépenses</h2>
+          <!-- Contenu du widget (graphique) -->
+        </div>
+  
+        <!-- Ajoutez autant de widgets que nécessaire -->
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      logout() {
+        // Ajoutez ici la logique de déconnexion (par exemple, redirigez vers la page de connexion)
+        // this.$router.push('/login');
+        this.$router.push('/login');
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Styles spécifiques au tableau de bord */
+  .dashboard-header {
+    background-color: #3498db;
+    color: #fff;
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  .dashboard-widgets {
+    display: flex;
+    justify-content: space-around;
+    margin-top: 20px;
+  }
+  
+  .dashboard-widget {
+    background-color: #ecf0f1;
+    padding: 20px;
+    margin: 10px;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+  
+  button {
+    background-color: #e74c3c;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+  }
+  
+  button:hover {
+    background-color: #c0392b;
+  }
+  </style>
+  
+  
