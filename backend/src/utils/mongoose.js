f297eb4cb29c3f6622beee190f418b460791df18
val-art01 <mayoujord@gmail.com>
2024-01-20 16:57:18 +0100

    using the online database

--- backend/src/utils/mongoose.js
+++ backend/src/utils/mongoose.js
@@ -4,12 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // const uri = `${process.env.DB_CONNECTION}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-// const uri = `${process.env.DB_CONNECTION}://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/?retryWrites=true&w=majority`;
-const uri = `${process.env.DB_CONNECTION}://mongodb:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+// const uri = `${process.env.DB_CONNECTION}://mongodb:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
+const uris = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const connectToDB = async () => {
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(uris);
     console.log("Connecté avec succès à la base de données");
   } catch (error) {
     console.error("Erreur lors de la connexion à la base de données : ", error);
